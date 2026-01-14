f69efdcd062d65a2e466dbfb58068b35b992f094
Stavros Sarikyriakidis <80428178+StavrosSar@users.noreply.github.com>
2026-01-14 18:08:27 +0200

    with a comment

--- ai_prototypes/fast_api_validation/main.py
+++ ai_prototypes/fast_api_validation/main.py
@@ -11,6 +11,7 @@ app.include_router(items_router)
 app.include_router(system_router)
 
 # Root endpoint
+# comment here
 @app.get("/")
 def root(a: int = Query(0), b: int = Query(0)):
     # Adds two numbers provided as query parameters
