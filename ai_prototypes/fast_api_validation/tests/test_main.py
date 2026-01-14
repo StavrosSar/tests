9cb3e821e8415f6d668eb3300c818c87957e1c84
Stavros Sarikyriakidis <80428178+StavrosSar@users.noreply.github.com>
2026-01-14 10:54:35 +0200

    change on test_main negative price

--- ai_prototypes/fast_api_validation/tests/test_main.py
+++ ai_prototypes/fast_api_validation/tests/test_main.py
@@ -13,7 +13,8 @@ def test_root():
 def test_create_item_success():
     r = client.post(
         "/items/",
-        json={"name": "Apple", "price": 2.5}
+        #a negative price
+        json={"name": "Apple", "price": -2.5}
     )
     assert r.status_code == 200
     assert r.json()["item"]["name"] == "Apple"
