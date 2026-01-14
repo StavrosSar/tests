a58a5cdfb881576115ba9a1711556c1213496745
Stavros Sarikyriakidis <80428178+StavrosSar@users.noreply.github.com>
2026-01-14 11:26:24 +0200

    PR_test1

--- ai_prototypes/fast_api_validation/tests/test_main.py
+++ ai_prototypes/fast_api_validation/tests/test_main.py
@@ -13,7 +13,7 @@ def test_root():
 def test_create_item_success():
     r = client.post(
         "/items/",
-        #a simple comment
+        #a simple comment!!
         json={"name": "Apple", "price": 2.5}
     )
     assert r.status_code == 200
